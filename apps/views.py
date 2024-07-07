37df687e7a4e5123b136d7e07f791559b9debb09
amritgiri <amritgiri5813@gmail.com>
2024-07-07 19:49:44 +0545

    Create multiple images upload handler

--- apps/views.py
+++ apps/views.py
@@ -11,3 +11,23 @@ class ImageCreateAPIView(viewsets.ModelViewSet):
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializers
     http_method_names = ["get","post","options"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = ImageSerializers(data=request.data)
+        if serializer.is_valid():
+            images = []
+            for image_data in request.FILES.getlist('image'):
+                serializer = ImageSerializers(data={'image': image_data})
+                if serializer.is_valid():
+                    serializer.save()
+                    images.append(serializer.data)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"images": images}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, *args, **kwargs):
+        images = ImageModel.objects.all()
+        serializer = ImageSerializers(images, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
