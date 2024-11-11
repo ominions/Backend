9786b483a5ecfcebcf31cd2501104f863992171c
amritgiri <amritgiri5813@gmail.com>
2024-11-11 22:14:20 +0545

    Add a method to upload and reterive ply files

--- apps/urls.py
+++ apps/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CreateView, ImageGETAPI, JSONUploadView, JSONRetriveView
+from .views import CreateView, ImageGETAPI, JSONUploadView, JSONRetriveView,PLYUploadView, PlyGetAPI
 
 router = routers.DefaultRouter()
 router.register(r"images", ImageGETAPI, basename="images")
-# router.register(r"upload", ImageCreateAPIView, basename="upload")
+router.register(r"ply", PlyGetAPI, basename="ply")
 
 urlpatterns = [
     path("api/", include(router.urls)),
@@ -13,4 +13,5 @@ urlpatterns = [
     path("api/cnn/", JSONUploadView.as_view(), name="upload_json"),
     path('api/cnn/jsondata/', JSONRetriveView.as_view(), name='get-all-json'),
     path('api/cnn/jsondata/<int:id>/', JSONRetriveView.as_view(), name='get-json-by-id'),
+    path('api/ply/upload', PLYUploadView.as_view(), name='ply-file-upload'),
 ]
